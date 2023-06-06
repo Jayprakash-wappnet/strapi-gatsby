8eb77e2f47cdb38fffb722a31059f627c157f9aa
Jayprakash-wappnet <jayprakash.wappnet@gmail.com>
2023-06-06 19:13:36 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Strapi-gatsby-test`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
